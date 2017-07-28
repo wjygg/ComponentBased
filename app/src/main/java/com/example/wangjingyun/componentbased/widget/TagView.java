5e81cf814e9b33b7d6c41af8152f7a17effe0fe8
商强龙 <82025361@qq.com>
2017-07-28 15:40:31 +0800

    流式布局

--- app/src/main/java/com/example/wangjingyun/componentbased/widget/TagView.java
+++ app/src/main/java/com/example/wangjingyun/componentbased/widget/TagView.java
@@ -5,6 +5,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * 自定义流式布局
@@ -15,6 +18,7 @@ public class TagView extends ViewGroup{
 
 
     public TagView(Context context) {
+
         this(context,null);
 
     }
@@ -59,7 +63,7 @@ public class TagView extends ViewGroup{
         for(int i=0;i<childCount;i++){
 
             //得到子view
-            View child = getChildAt(0);
+            View child = getChildAt(i);
             //测量 子view
             measureChild(child,widthMeasureSpec,heightMeasureSpec);
             //获取layout params 获取 子控件 margin 值
@@ -91,7 +95,6 @@ public class TagView extends ViewGroup{
                 //最高的 控件
                 lineHeight=Math.max(lineHeight,childHeight);
 
-
             }
 
             //累加最后一个的高度
@@ -108,15 +111,110 @@ public class TagView extends ViewGroup{
 
     }
 
+    //记录所有的 控件
+    private List<List<View>> mAllViews=new ArrayList<List<View>>();
+
+    //记录每行的高
+    private List<Integer> mHeight=new ArrayList<Integer>();
+
     @Override
     protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
 
-        //
-        
+        mAllViews.clear();
+        mHeight.clear();
+
+        //总宽度
+        int width=getWidth();
+
+        //一行累加宽度
+        int lineWidth=0;
+        //一行最高的宽度
+        int lineHeight=0;
+
+        int childCount=getChildCount();
+
+        List<View> views=new ArrayList<View>();
+
+        for(int j=0;j<childCount;j++){
+
+            View child=getChildAt(j);
+
+            MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+            int childWidth=child.getMeasuredWidth()+layoutParams.leftMargin+layoutParams.rightMargin;
+
+            int childHeight=child.getMeasuredHeight()+layoutParams.topMargin+layoutParams.bottomMargin;
+
+            if(lineWidth+childWidth>width){
+
+                //存储每行最高的
+                mHeight.add(lineHeight);
+
+                mAllViews.add(views);
+
+                //添加一行view
+                views=new ArrayList<View>();
+
+                views.add(child);
+                //换行
+                lineWidth=childWidth;
+                //行高重新赋值
+                lineHeight=childHeight;
+
+            }else{
+                //累加宽度
+
+                lineWidth=lineWidth+childWidth;
+
+                lineHeight=Math.max(lineHeight,childHeight);
+
+                views.add(child);
+
+            }
+        }
+
+            // 记录最后一行  
+                mHeight.add(lineHeight);
+                mAllViews.add(views);
+
+        int left=0;
+        int top=0;
+
+        //总行数
+        int mAllViewsSize=mAllViews.size();
+        for(int z=0;z<mAllViewsSize;z++){
+
+            //每一行的 views
+            views = mAllViews.get(z);
+
+            //每行最高
+            lineHeight=mHeight.get(z);
+
+            for(int k=0;k<views.size();k++){
+
+                View child=views.get(k);
+
+                if(child.getVisibility()==View.GONE){
+
+                    continue;
+                }
+                MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+
+                int lc=left+layoutParams.leftMargin;
+                int tc=top+layoutParams.topMargin;
+                int rc=lc+child.getMeasuredWidth();
+                int bc=tc+child.getMeasuredHeight();
+
+                child.layout(lc,tc,rc,bc);
+
+                left+=child.getMeasuredWidth()+layoutParams.rightMargin+
+                        layoutParams.leftMargin;
+
+            }
+            left=0;
+            top+=lineHeight;
+        }
     }
-
-
-
     // 返回系统的layoutParams
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
