150d5a57b868214ffffd39485b9eb94abdf2e8c7
wangjingyun <1299880641@qq.com>
2019-01-22 18:33:24 +0800

    retrofit添加

---
+++ componentbasesdk/src/main/java/com/example/wangjingyun/componentbasesdk/retrofit/Transformers.java
@@ -0,0 +1,12 @@
+package com.example.wangjingyun.componentbasesdk.retrofit;
+
+import android.database.Observable;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Response;
+
+public class Transformers {
+
+
+}
