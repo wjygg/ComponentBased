b9941aeb701aa5142a6e0f3c5fc60da1a7d45e67
wangjingyun <wangjingyun@hztianque.com>
2019-08-02 17:14:40 +0800

    画笔渲染

---
+++ app/src/main/java/com/example/wangjingyun/componentbased/widget/pixeladaptation/CanvasStatus.kt
@@ -0,0 +1,27 @@
+package com.example.wangjingyun.componentbased.widget.pixeladaptation
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class CanvasStatus : View {
+
+    constructor(mContext:Context):super(mContext){
+
+    }
+
+    constructor(mContext:Context,attributeSet: AttributeSet):super(mContext,attributeSet){
+
+    }
+    constructor(mContext:Context,attributeSet: AttributeSet,defStyleAttr:Int):super(mContext,attributeSet,defStyleAttr){
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+
+    }
+
+}
